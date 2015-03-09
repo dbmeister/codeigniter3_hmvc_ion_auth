0f09e2193ceb1827097a5fba466033bb8f780b6b
root <root@webserver.(none)>
2015-03-09 22:40:38 +0000

    Templates module plus Minor fixes

--- application/modules/auth/config/ion_auth.php
+++ application/modules/auth/config/ion_auth.php
@@ -83,7 +83,7 @@ $config['salt_prefix']    = version_compare(PHP_VERSION, '5.3.7', '<') ? '$2a$' 
  | appropriately. If this variable set to 0, there is no maximum.
  */
 $config['site_title']                 = "Example.com";       // Site Title, example.com
-$config['admin_email']                = "jasonb@mayflex.com"; // Admin Email, admin@example.com
+$config['admin_email']                = "admin@example.com"; // Admin Email, admin@example.com
 $config['default_group']              = 'members';           // Default group, use name
 $config['admin_group']                = 'admin';             // Default administrators group, use name
 $config['identity']                   = 'email';             // A database column which is used to login with
